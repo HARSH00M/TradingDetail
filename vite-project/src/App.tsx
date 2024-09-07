f2fb47c5932541b0b85aa7f0733e248a5d3e6872
Harsh <harsh10mahobia@gmail.com>
2024-09-07 17:23:26 +0530

    changes for login

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,10 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Details from './pages/details';
 import NoPage from './pages/nopage';
-import Layout from './pages/layout';
+// import Layout from './pages/layout';
 import Allcompanies from './pages/companies';
-import ProtectedRoute from './components/auth/protected';
-import AuthProvider  from './components/auth/useAuth';
+// import ProtectedRoute from './components/auth/protected';
+// import AuthProvider  from './components/auth/useAuth';
 import Login from './pages/login';
 
 const App: React.FC = () => {
